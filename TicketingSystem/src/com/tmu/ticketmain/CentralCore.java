2c7c786107982a42365b7ecc2a45002f7c03259f
tjjay17 <56407501+tjjay17@users.noreply.github.com>
2023-03-07 21:51:16 -0500

    login and logout added

--- TicketingSystem/src/com/tmu/ticketmain/CentralCore.java
+++ TicketingSystem/src/com/tmu/ticketmain/CentralCore.java
@@ -26,23 +26,39 @@ public class CentralCore {
         }
         
         String userInput = "";
+        boolean firstRun = true;
         try(BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))){
             //prompts can be changed
             System.out.println("Welcome to the ticket system");
-            System.out.println("Enter a command to begin");
+            System.out.println("Enter a command, or 'c' for a list of commands.");
+
+
             while((userInput = stdIn.readLine()) != null){
                 //readin user inputs and assess them
-                if(userInput.equals("login")){
+                
+                if(userInput.equals("Login")){
                     System.out.println("Please enter your username.");
                     String loginUser = stdIn.readLine();
                     if(login(loginUser)){
                         System.out.println("Login successful!");
                     }else{
-                        System.out.println("Cannot find a user with that name, please try again");
+                        System.out.println("Cannot find a user with that username");
                     }
-                }else if(userInput.equals("quit")){
+                }else if(userInput.equals("Quit")){
                     break;
+                }else if(userInput.equals("c")){
+                    getUserOperations();
+                }else if(userInput.equals("Logout")){
+                    logout();
+                }else{
+                    System.out.println("Command not understood");
                 }
+
+                if(firstRun){
+                    firstRun = false; 
+                 }else{
+                     System.out.println("Enter a command, or 'c' for a list of commands.");
+                 }
             }        
         }catch (IOException e){
             System.out.println(e.getMessage());
@@ -61,13 +77,20 @@ public class CentralCore {
         return userFound;
     }
     
-    public void logout(){
+    public static void logout(){
+        activeUser = null;
+        System.out.println("Logout successful.");
     }
     
     public void createDailyTransaction(int transCode, String userName, String userType, int userCredit){
     }
     
-    public void getUserOperations(String activeUser, String userType){
+    public static void getUserOperations(){
+        if(activeUser == null){
+            System.out.println("Login, Quit");
+        }else if(activeUser != null){
+            System.out.println("Buy, Sell, Logout, Quit, Create, Delete, Addcredit, Refund");
+        }
     }
     
     public static List<Ticket> getTickets(){
