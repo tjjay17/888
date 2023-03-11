06872963f0d423d5129e9ab09324e60de995adc5
KringeKorner <80902587+KringeKorner@users.noreply.github.com>
2023-03-10 20:35:29 -0500

    Updated Admin
    Updated Admin. At the time CentralCore did not read in users into list, so for sake of testing all reads and writes were done in the isolated test rig via directly filling a userlist in each required context by reading/writing a users.txt file. Relevant comments are present where needed for explanations. Refactoring to read from arraylist is simple.

--- TicketingSystem/src/com/tmu/ticketmain/Admin.java
+++ TicketingSystem/src/com/tmu/ticketmain/Admin.java
@@ -11,10 +11,158 @@ public class Admin extends User{
     
     public boolean getUser(String target){
         //Placeholder function. Used to retrieve user data from user list.
-        String formatted = String.format("Found user %s", target);
-        System.out.println(formatted);
-        
-        return true;
+        int i = 0;
+        boolean check = false;
+        boolean localCheck;
+        String outputMessage = "";
+        List userList = new ArrayList();
+        //create list while not reading from array list in CentralCore
+        try{
+            File users = new File("users.txt");
+            Scanner userReader = new Scanner(users);
+            while (userReader.hasNextLine()) {
+                String readIn = userReader.nextLine();
+                String temp = readIn.split(" ")[0];
+                userList.add(temp);
+            }
+            do{
+                localCheck = target.equals(userList.get(i));
+//                System.out.println(userList.get(i));
+                if(localCheck == true){
+                    outputMessage = String.format("User \"%s\" successfully found.", target);
+                    System.out.println(outputMessage);
+                    check = true;
+                    break;
+                }
+                else if(localCheck == false && i<userList.size()-1){
+                    i++;
+                    check = false;
+                }
+                else{
+                    System.out.println("No such user exists in system.");
+                    check = false;
+                    break;
+                }
+            }
+            while(check != true);
+            userReader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+        return check;
+    }
+    
+    public boolean checkUsername(String testUsername){
+        int i = 0;
+        boolean check = false;
+        boolean localCheck;
+        List userList = new ArrayList();
+        //create list while not reading from array list in CentralCore
+        try{
+            File users = new File("users.txt");
+            Scanner userReader = new Scanner(users);
+            while (userReader.hasNextLine()) {
+                String readIn = userReader.nextLine();
+                String temp = readIn.split(" ")[0];
+                userList.add(temp);
+            }
+            do{
+                localCheck = testUsername.equals(userList.get(i));
+                if(localCheck == true){
+                    System.out.println("User already exists.");
+                    check = false;
+                    break;
+                }
+                else if(localCheck == false && i<userList.size()-1){
+                    i++;
+                    check = false;
+                }
+                else{
+                    System.out.println("No such user exists in system.");
+                    check = true;
+                    break;
+                }
+            }
+            while(check != true);
+            userReader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+        return check;
+    }
+    
+    public String formatter(String username){
+        String tempFormattedUser = username;
+        String formattedUser;
+        int length;
+        length = tempFormattedUser.length();
+        do{
+            formattedUser = tempFormattedUser + " ";
+            tempFormattedUser = formattedUser;
+            length = tempFormattedUser.length();
+        }
+        while(length!=15);
+        return formattedUser;
+    }
+    
+    public static int getPosition(String target){
+        int foundAt = 0;
+        int i = 0;
+        boolean check = false;
+        boolean localCheck;
+        String outputMessage = "";
+        List userList = new ArrayList();
+        //create list while not reading from array list in CentralCore
+        try{
+            File users = new File("users.txt");
+            Scanner userReader = new Scanner(users);
+            while (userReader.hasNextLine()) {
+                String readIn = userReader.nextLine();
+                String temp = readIn.split(" ")[0];
+                userList.add(temp);
+            }
+            do{
+                localCheck = target.equals(userList.get(i));
+//                System.out.println(userList.get(i));
+                if(localCheck == true){
+                    System.out.println(outputMessage);
+                    check = true;
+                    foundAt = i;
+                    break;
+                }
+                else if(localCheck == false && i<userList.size()-1){
+                    i++;
+                    check = false;
+                }
+                else{
+                    check = false;
+                    break;
+                }
+            }
+            while(check != true);
+            userReader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found.");
+        }
+        System.out.println(foundAt);
+        return foundAt;
+    }
+    
+    public static String creditFormatter(Double credits){
+        String tempFormattedCredits = Double.toString(credits);
+        String formattedCredits;
+        int length;
+        length = tempFormattedCredits.length();
+        do{
+            formattedCredits = tempFormattedCredits + "0";
+            tempFormattedCredits = formattedCredits;
+            length = tempFormattedCredits.length();
+        }
+        while(length!=9);
+        return formattedCredits;
     }
     
     public void getRefundRequests(){
@@ -22,23 +170,123 @@ public class Admin extends User{
         System.out.println("No refund requests to be retrieved.");
     }
     
+    @Override
+    public void addCredit(){
+        //basic requirements
+        String username;
+        double updatedCredits = 0.0;
+        double credits = 0.0;
+        //specific for function
+        boolean addCredits_active = true;
+        Scanner userInput = new Scanner(System.in);
+        boolean validInput = false;
+        boolean check = false;
+        int position = 0;
+        double oldValue = 0.0;
+        double comparator = 0.0;
+        
+        while(addCredits_active){
+            System.out.println("Enter target username:");
+            //checking username
+            username = userInput.nextLine();
+            check = getUser(username);
+            while(username.length() > 15 || check == false){
+                System.out.println("Invalid entry for username. Please resubmit.");
+                username = userInput.nextLine();
+                check = getUser(username);
+            }
+            //Enter credit addition
+            System.out.println("Enter the credit amount");
+            do {
+                if(userInput.hasNextDouble()){
+                    credits = userInput.nextDouble();
+                    comparator = Double.compare(credits, 1000.00);
+                    if(comparator > 0){
+                        System.out.println("value in excess of 1000.00. Please resubmit.");
+                        validInput = false;
+                        userInput.next();
+                        comparator = Double.compare(credits, 1000.00);
+                    }
+                    else{
+                        validInput = true;
+                        break;
+                    }
+                }
+                else{
+                    System.out.println("Invalid entry for credit. Please resubmit.");
+                    validInput = false;
+                    userInput.next();
+                }
+            }
+            while(!(validInput));
+            //finding position
+            if(check == true){
+                position = getPosition(username);
+                //updating the credits
+                try{
+                    int i = 0;
+                    List userList = new ArrayList();
+                    //create list while not reading from array list in CentralCore
+                    File users = new File("users.txt");
+                    Scanner userReader = new Scanner(users);
+                    //readIn while userlist not ready in CentralCore
+                    while(userReader.hasNextLine()) {
+                        String readIn = userReader.nextLine();
+                        userList.add(readIn);
+                    }
+                    userReader.close();
+                    //Modifying the credits
+                    String modifiable = userList.get(position).toString();
+                    String coreChunk = modifiable.substring(0,20);
+                    oldValue = Double.parseDouble(modifiable.substring(20,29));
+                    updatedCredits = oldValue + credits;
+                    String convertedCredits = creditFormatter(updatedCredits);
+                    modifiable = coreChunk + convertedCredits;
+                    userList.set(position, modifiable);
+                    System.out.println(userList);
+                    //writing while userlist not ready in CentralCore
+                    FileWriter fileWriter = new FileWriter("users.txt");
+                    int size = userList.size();
+                    for (i=0;i<size;i++) {
+                        String str = userList.get(i).toString();
+                        fileWriter.write(str);
+                        if(i < size - 1)
+                            fileWriter.write("\n");
+                    }
+                    fileWriter.close();
+                    //Success message
+                    String deletionMessageSuccess = String.format("Updated credits for user with username \"%s\" to %f", username, updatedCredits);
+                    System.out.println(deletionMessageSuccess);
+                }
+                catch(IOException e){
+                    System.out.println("File not found.");
+                }
+            }
+            addCredits_active = false;
+        }
+    }
+    
     public void createUser(){
         //basic requirements
         String username;
         String type;
         double credits = 0;
+        
         //specific for function
         boolean createUser_active = true;
         Scanner userInput = new Scanner(System.in);
         boolean validInput = false;
+        boolean check;
 
         while(createUser_active){
             //filling data
             System.out.println("Enter a new username");
             username = userInput.nextLine();
-            while(username.length() > 15){
+            check = checkUsername(username);
+            while(username.length() > 15 || check == false){
                 System.out.println("Invalid entry for username. Please resubmit.");
                 username = userInput.nextLine();
+                check = checkUsername(username);
             }
             
             System.out.println("Enter the user type");
@@ -67,39 +315,79 @@ public class Admin extends User{
             userInput.close();
             
             //attempt to bundle data
+            //Write to file directly while userlist not made in CentralCore
             try{
-                String formatted = String.format("Created user %s of type %s with balance %.2f", username, type, credits);
-                System.out.println(formatted);
+                FileWriter fw = new FileWriter("users.txt", true); //the true will append the new data
+                String formattedUsername = formatter(username);
+                String formatted = String.format("%s  %s %.2f\n", formattedUsername, type.toUpperCase(), credits);
+                fw.write(formatted);
+                System.out.print(formatted);
+                fw.close();
             }
             catch (Exception e) {
-		            System.out.println("Exception caught. Reattempt submission.");
+		System.out.println("Exception caught. Reattempt submission.");
 	    }
             //attempt to add to user file
             //-->To be added in later version
             createUser_active = false;
         }
-        
     }
     
     public void deleteUser(){
         //Preliminary version. No reading from UserList currently incorporated. Future version will update function to search userList for matching user.
         String username;
         boolean isFound;
+        //Specific to function
+        boolean check;
+        int position = 0;
         
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter user to be deleted");
         username = userInput.nextLine();
-        while(username.length() > 15){
+        check = getUser(username);
+        while(username.length() > 15 || check == false){
             System.out.println("Invalid entry for username. Please resubmit.");
             username = userInput.nextLine();
+            check = getUser(username);
         }
-        isFound = getUser(username);
+        isFound = check;
         if(isFound == true){
-            String deletionMessageSuccess = String.format("Deleted user with username %s", username);
-            System.out.println(deletionMessageSuccess);
+            position = getPosition(username);
+        }
+        if(isFound == true){
+            try{
+                int i = 0;
+                List userList = new ArrayList();
+                //create list while not reading from array list in CentralCore
+                File users = new File("users.txt");
+                Scanner userReader = new Scanner(users);
+                //readIn while userlist not ready in CentralCore
+                while(userReader.hasNextLine()) {
+                    String readIn = userReader.nextLine();
+                    userList.add(readIn);
+                }
+                userReader.close();
+                userList.remove(position);
+                //writing while userlist not ready in CentralCore
+                FileWriter fileWriter = new FileWriter("users.txt");
+                int size = userList.size();
+                for (i=0;i<size;i++) {
+                    String str = userList.get(i).toString();
+                    fileWriter.write(str);
+                    if(i < size - 1)
+                        fileWriter.write("\n");
+                }
+                fileWriter.close();
+                //Success message
+                String deletionMessageSuccess = String.format("Deleted user with username \"%s\"", username);
+                System.out.println(deletionMessageSuccess);
+            }
+            catch(IOException e){
+                System.out.println("File not found.");
+            }
         }
         else{
-            String deletionMessageFail = String.format("Failed to find user with username %s", username);
+            String deletionMessageFail = String.format("Failed to delete user \"%s\".", username);
             System.out.println(deletionMessageFail);
         }
         
@@ -217,7 +505,6 @@ public class Admin extends User{
                     System.out.println(successfulAdded);
                     String successfulSubtracted = String.format("Successfully subtracted %.2f from %s wallet, resulting in %.2f",refundCredit, seller, sellerCredit);
                     System.out.println(successfulSubtracted);
-                    
                     break;
             }
         }
